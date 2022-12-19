03738685d1b6d591fc4a63177542f052443b452e
Ntshangase <simphiwe323i@gmail.com>
2022-12-19 23:34:42 +0200

    feat: Add to listt function

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent {
+  newTodo: string = '';
+  todos: string[] = [];
 
+  addTodo() {
+    this.todos.push(this.newTodo);
+    this.newTodo = '';
+  }
 }
